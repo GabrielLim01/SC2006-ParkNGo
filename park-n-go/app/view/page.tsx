606a259781a332b11717d438fdff8707312e7bf7
80050863+sentheta@users.noreply.github.com <80050863+sentheta@users.noreply.github.com>
2024-10-11 14:42:39 +0800

    created info card in view/, barebone implementation of saved

--- park-n-go/app/view/page.tsx
+++ park-n-go/app/view/page.tsx
@@ -6,9 +6,11 @@ import { Navbar } from "flowbite-react";
 
 // Bootstrap components
 import { Button, Col, Container, Row } from "react-bootstrap";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 // Additional pages
+// import {CarparkTable} from "./carparkInformation/carparkTable"
+import "./index.css";
 // import { CarparkList } from "./CarparkList";
 
 // ReactSearchKit component
@@ -16,35 +18,46 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
 
+  // todo: fetch this particular carpark info from somewherw??!?!
+  const carparkID = "NNDN"
+  const carparkAddr = "Nanyang Drive"
+  const availLots = 192
+  const carparkType = "MULTI-STOREY"
+  const freeParking = true
+  const nightParking = false
+  const shortTerm = "7AM-10.30PM"
+  const gantryHeight = 4.50
+
   // todo: actually save in a local database
-  const [saved, setSaved] = useState([])
+  const [saved, setSaved] = useState(() => {
+    const localValue = localStorage.getItem("SAVED")
+    if(localValue==null) return []
+    return JSON.parse(localValue)
+  })
+  useEffect(() => {
+    localStorage.setItem("SAVED", JSON.stringify(saved))
+  }, [saved])
+
 
   // add a carpark information into the list
-  // function addSaved(id, address, xCoord, yCoord, cpType, shortTermParking, freeParking, nightParking, gantryHeight, availLots) {
-  //   setSaved(currentSaved => {
-  //     return [
-  //       ...currentSaved,
-  //       { id, address, xCoord, yCoord, cpType, shortTermParking, freeParking, nightParking, gantryHeight, availLots }
-  //     ]
-  //   })
-  // }
+  function addSaved(id){
+    setSaved(currentSaved => {
+      return [...currentSaved, id]
+    })
+  }
+  function deleteSaved(id) {
+    setSaved(currentSaved => {
+      return currentSaved.filter(item => item !== id)
+    })
+  }
 
-  // delete carpark information from the list
-  // function deleteSaved(id) {
-  //   setSaved(currentSaved => {
-  //     return currentSaved.filter(item => item.id !== id)
-  //   })
-  // }
+  
 
   return (
     <div>
       <Navbar fluid rounded>
         <Navbar.Brand>
-          <img
-            src="/ParkNGo_Icon.png"
-            className="mr-3 h-6 sm:h-9"
-            alt="ParkNGo Logo"
-          />
+          <img src="/ParkNGo_Icon.png" className="mr-3 h-6 sm:h-9" alt="ParkNGo Logo"/>
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             ParkNGo
           </span>
@@ -60,15 +73,48 @@ export default function Home() {
           <Navbar.Link href="/navbars">Contact</Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
+
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-        {/* <ReactSearchKit>
-          <SearchBar />
-        </ReactSearchKit> */}
+        
 
-        {
+        <div className="card">
+          <p><b>{carparkID}</b></p>
+          <p>{carparkAddr}</p>
+          <p>{availLots} available lots</p>
+          <p>Carpark type: {carparkType}</p>
+          <p>Short term: {shortTerm}</p>
+          <p>Gantry height: {gantryHeight}m</p>
           
-        }
+          {freeParking ? 
+            <p style={{color:'green'}}>Free parking</p>
+            :
+            <p style={{color:'red'}}>No free parking</p>
+          }
+
+          {nightParking ? 
+            <p style={{color:'green'}}>Night parking</p>
+            :
+            <p style={{color:'red'}}>No night parking</p>
+          }
+
+          {/* {true? */}
+          {saved.includes(carparkID)?
+            <button onClick={e=>deleteSaved(carparkID)}
+                    style={{color:'red', backgroundColor:'hsla(350,100%,72%,0.5)'}}
+                    className="btn">
+              Unsave
+            </button>
+            :
+            <button onClick={e=>addSaved(carparkID)}
+                    style={{color:'blue', background:'hsl(200,100%,50%,0.5)'}}
+                    className="btn">
+              Save
+            </button>
+          }
+          
+
+        </div>
 
       </main>
     </div>
