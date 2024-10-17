d32a0ceb9e7624395cccf27a433be851caf41835
Fay <faybeata.k@gmail.com>
2024-10-17 22:57:39 +0800

    feat: added get current location (geolocation) /view

--- park-n-go/app/MainLayout.tsx
+++ park-n-go/app/MainLayout.tsx
@@ -7,7 +7,7 @@ import { Navbar } from "flowbite-react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen">
+    <div className="h-screen overflow-scroll">
       <Navbar fluid className="sm:px-32 bg-mainBlue py-4 h-28 w-full flex-col content-center justify-items-start">
         {/* <Image
           src="/images/ParkNGo_Icon.png"
@@ -17,26 +17,26 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         /> */}
         <span className="text-left text-5xl font-oswald font-bold text-white">
           <h1>ParkNGo</h1>
-          <h2 className="text-base -font-sans-serif-3 font-normal">A carpark locator application</h2>
+          <h2 className="text-base font-sans-serif-3 font-normal">A carpark locator application</h2>
         </span>
         <Navbar.Toggle />
         <Navbar.Collapse className="text-lg font-bold text-white">
           <Navbar.Link href="/dashboard">
-            <h1 className="self-center whitespace-nowrap text-xl font-semibold text-white" >Home</h1>
+            <h1 className="self-center whitespace-nowrap text-lg font-semibold text-white" >Home</h1>
           </Navbar.Link>
           <Navbar.Link href="/search">
-            <h1 className="self-center whitespace-nowrap text-xl font-semibold text-white" >Search</h1>
+            <h1 className="self-center whitespace-nowrap text-lg font-semibold text-white" >Search</h1>
           </Navbar.Link>
           <Navbar.Link href="/about">
-            <h1 className="self-center whitespace-nowrap text-xl font-semibold text-white" >About</h1>
+            <h1 className="self-center whitespace-nowrap text-lg font-semibold text-white" >About</h1>
           </Navbar.Link>
           <Navbar.Link href="/contact">
-            <h1 className="self-center whitespace-nowrap text-xl font-semibold text-white" >Contact</h1>
+            <h1 className="self-center whitespace-nowrap text-lg font-semibold text-white" >Contact</h1>
           </Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
 
-      <div className="mx-32 mt-16">{children}</div>
+      <div className="mx-32 mt-4">{children}</div>
 
       {/* Animation */}
       {/* <div className="box">
--- park-n-go/app/view/page.tsx
+++ park-n-go/app/view/page.tsx
@@ -19,7 +19,7 @@ import "./index.css";
 // import { ReactSearchKit, SearchBar } from 'react-searchkit';
 
 // Google API
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import { AdvancedMarker, APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
 const API_KEY = globalThis.NEXT_PUBLIC_GMAPS_API_KEY ?? (process.env.NEXT_PUBLIC_GMAPS_API_KEY as string);
 
 export default function Home() {
@@ -33,9 +33,14 @@ export default function Home() {
   const nightParking = false
   const shortTerm = "7AM-10.30PM"
   const gantryHeight = 4.50
-  // convert fromt SG latitude longitude to global system
-  const lati = 1.3521
-  const long = 103.8198
+
+  const location = {
+    lat : 1.3521,
+    long : 103.8198
+  }
+  //state
+  const [position_lat, setPosition_lat] = useState(1.3521);
+  const [position_long, setPosition_long] = useState(103.8198);
 
   // todo: actually save in a local database
   const [saved, setSaved] = useState(() => {
@@ -43,8 +48,54 @@ export default function Home() {
     if (localValue == null) return []
     return JSON.parse(localValue)
   })
+
+  //find loc - gmap
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+  //find loc - gmap
+  function success(pos) {
+    var crd = pos.coords;
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+    setPosition_lat(crd.latitude)
+    setPosition_long(crd.longitude)
+  }
+  //find loc - gmap
+  function errors(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
   useEffect(() => {
     localStorage.setItem("SAVED", JSON.stringify(saved))
+    
+    //get location
+    if (navigator.geolocation) {
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          console.log(result);
+          if (result.state === "granted") {
+            //If granted then you can directly call your function here
+            navigator.geolocation.getCurrentPosition(success, errors, options);
+          } else if (result.state === "prompt") {
+            //If prompt then the user will be asked to give permission
+            navigator.geolocation.getCurrentPosition(success, errors, options);
+            //navigator.geolocation.getCurrentPosition((position) => {
+              //setPosition_lat(position.coords.latitude)
+              //setPosition_long(position.coords.longitude)
+              //})
+          } else if (result.state === "denied") {
+            //If denied then you have to show instructions to enable location
+          }
+        });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
   }, [saved])
 
 
@@ -59,71 +110,106 @@ export default function Home() {
       return currentSaved.filter(item => item !== id)
     })
   }
+
   //map
-  const DMap = () => (
+  const DMap = ({loc}) => (
     <APIProvider apiKey={API_KEY}>
       <Map
-        style={{ width: '70vw', height: '80vh' }}
-        defaultCenter={{ lat: lati, lng: long }}
+        mapId={'bf51a910020fa25a'}
+        //style={{ width: '70vw', height: '80vh' , alignItems:"center"}}
+        defaultCenter={{ lat: loc.lat, lng: loc.long }}
         defaultZoom={15}
         gestureHandling={'greedy'}
-        disableDefaultUI={true}>
+        disableDefaultUI={true}
+      >
         {/* simple marker */}
         <Marker
-          position={{ lat: lati, lng: long }}
+          position={{ lat: loc.lat, lng: loc.long }}
           clickable={true}
           onClick={() => alert('marker was clicked!')}
           title={'clickable google.maps.Marker'}
         />
+        <AdvancedMarker
+          position={{lat: position_lat, lng: position_long}}
+          title={'AdvancedMarker with custom html content.'}>
+          <div style={{
+                width: 24,
+                height: 24,
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                background: '#6390ff',
+                border: '5px solid #FFFFFF',
+                borderRadius: '50%',
+              }}>
+            <div
+              style={{
+                opacity: 0.3,
+                width: 36,
+                height: 36,
+                position: 'absolute',
+                top: -11,
+                left: -11,
+                background: '#6390ff',
+                borderRadius: '50%',
+                zIndex: -1,
+                
+              }}></div>
+          </div>
+
+
+        </AdvancedMarker>
       </Map>
+      
     </APIProvider>
   );
 
   return (
     <Layout>
       <Head>
-        <title>Voew</title>
+        <title>View</title>
+
       </Head>
 
-      <div className="rounded-md object-contain">
-        <DMap />
-      </div>
+      <div className="h-screen flex justify-center pt-10">
+        <div className='w-1/2 h-4/5 items-center rounded-lg mr-8'><DMap loc={location}/></div>
 
-      <div className="card text-black">
-        <p><b>{carparkID}</b></p>
-        <p>{carparkAddr}</p>
-        <p>{API_KEY}</p>
-        <p>{availLots} available lots</p>
-        <p>Carpark type: {carparkType}</p>
-        <p>Short term: {shortTerm}</p>
-        <p>Gantry height: {gantryHeight}m</p>
+        <div className="card text-black w-1/3 h-4/5">
+          <p><b>{carparkID}</b></p>
+          <p>{carparkAddr}</p>
+          <p>{availLots} available lots</p>
+          <p>Carpark type: {carparkType}</p>
+          <p>Short term: {shortTerm}</p>
+          <p>Gantry height: {gantryHeight}m</p>
 
-        {freeParking ?
-          <p style={{ color: 'green' }}>Free parking allowed</p>
-          :
-          <p style={{ color: 'red' }}>No free parking</p>
-        }
+          {freeParking ?
+            <p style={{ color: 'green' }}>Free parking allowed</p>
+            :
+            <p style={{ color: 'red' }}>No free parking</p>
+          }
 
-        {nightParking ?
-          <p style={{ color: 'green' }}>Night parking allowed</p>
-          :
-          <p style={{ color: 'red' }}>No night parking</p>
-        }
+          {nightParking ?
+            <p style={{ color: 'green' }}>Night parking allowed</p>
+            :
+            <p style={{ color: 'red' }}>No night parking</p>
+          }
 
-        {/* {true? */}
-        {saved.includes(carparkID) ?
-          <button onClick={e => deleteSaved(carparkID)}
-            style={{ color: 'red', backgroundColor: 'hsla(350,100%,72%,0.5)' }}
-            className="btn">
-            Unsave
-          </button>
-          :
-          <button onClick={e => addSaved(carparkID)}
-            style={{ color: 'blue', background: 'hsl(200,100%,50%,0.5)' }}
-            className="btn">
-            Save
-          </button>
-        }
+          {/* {true? */}
+          {saved.includes(carparkID) ?
+            <button onClick={e => deleteSaved(carparkID)}
+              style={{ color: 'red', backgroundColor: 'hsla(350,100%,72%,0.5)' }}
+              className="btn">
+              Unsave
+            </button>
+            :
+            <button onClick={e => addSaved(carparkID)}
+              style={{ color: 'blue', background: 'hsl(200,100%,50%,0.5)' }}
+              className="btn">
+              Save
+            </button>
+          }
+        </div>
+
       </div>
     </Layout>
   );
